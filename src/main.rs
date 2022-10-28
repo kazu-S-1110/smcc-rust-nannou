6b85c9e4c13de44ed848d09addc3ea054014f5b9
kazu-S-1110 <xtpgdmw3690@gmail.com>
2022-10-28 12:00:19 +0900

    smcc done

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,47 @@
-use std::env;
+use nannou::prelude::*;
 
-mod smcc;
+static RADIUS: f32 = 25.0;
 
 fn main() {
-    // smcc::stairs::stairs(1);
-    // smcc::stairs::stairs(2);
-    smcc::stairs::stairs(3);
-    smcc::stairs::stairs(10);
-    smcc::stairs::stairs(20);
+    // nannou::app(model).update(update).simple_window(view).run()
+    nannou::sketch(view).size(100, 100).run();
+}
+
+struct Model {}
+
+fn model(_app: &App) -> Model {
+    Model {}
+}
+
+fn update(_app: &App, _model: &mut Model, _update: Update) {}
+
+fn view(app: &App, frame: Frame) {
+    let draw = app.draw();
+    draw.background().color(WHITE);
+
+    for y in -25..=0 {
+        for x in -25..=0 {
+            let dx = x as f32 - 0.0;
+            let dy = y as f32 - 0.0;
+            let dist = (dx.powf(2.0) + dy.powf(2.0)).sqrt();
+
+            let diff = dist - RADIUS as f32;
+
+            let abs_diff = abs(diff);
+            match diff {
+                -1.0..=1.0 => draw
+                    .rect()
+                    .x_y(x as f32, y as f32)
+                    .w_h(1.0, 1.0)
+                    .color(rgb(abs_diff, abs_diff, abs_diff)),
+                _ => draw
+                    .rect()
+                    .x_y(x as f32, y as f32)
+                    .w_h(1.0, 1.0)
+                    .color(WHITE),
+            };
+        }
+    }
+
+    draw.to_frame(app, &frame).unwrap()
 }
